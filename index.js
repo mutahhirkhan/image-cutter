b1dcea0ec2c29aefbeb7d8909594a2d660046273
mutahhirkhan <mutahhir199@gmail.com>
2022-10-19 23:58:57 +0500

    script cleanups

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function imageCutter(noOfSubImages = 13) {
     //load image to cut
     let image1 = await jimp.read('img.jpg');
     let imageArr = [];
-    //create array of images clones
+    //create array of images clones till noOfSubImages times
     for (let i = 0; i < noOfSubImages; i++) {
         imageArr.push(image1.clone());
     }
@@ -13,7 +13,7 @@ async function imageCutter(noOfSubImages = 13) {
     //get height and width of output single image, 1 pixel is subtracted due to 0 -> 1 indexing
     //single image height will be total height divided by number of sub-images
     let singleImageHeight = parseInt((image1.getHeight())/noOfSubImages);
-    let singleImageWidth = image1.getWidth();
+    let singleImageWidth = parseInt(image1.getWidth());
 
     //crop these images into defined equale parts
     ///@params: x, y, width, height
@@ -22,7 +22,7 @@ async function imageCutter(noOfSubImages = 13) {
     ///@param width: width of the cropped image
     ///@param height: height of the cropped image
     imageArr.forEach((img, index) => {
-        img.crop(0, parseInt(singleImageHeight*index), parseInt(singleImageWidth), singleImageHeight);
+        img.crop(0, (singleImageHeight*index), singleImageWidth, singleImageHeight);
     })
 
     //write the cropped images to current directory
