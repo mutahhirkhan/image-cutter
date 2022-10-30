70dad27a81ccca87b9fb7b0a21a7db270f39572d
mutahhirkhan <mutahhir199@gmail.com>
2022-10-30 15:10:17 +0500

    final: all work done and safely backedup to drive.

--- Combine.js
+++ Combine.js
@@ -1,48 +1,29 @@
 // merge PDF's page # 1,2,3,4 into one page and page # 5,6,7,8 into another page
-const PDFDocument = require("pdfkit");
 const fs = require("fs");
 const { createPDF, addMarginPDF } = require("pdf-actions");
+const { Blob } = require("buffer");
 
-function usingPDFKIT() {
-	const doc = new PDFDocument();
-	const pdfDoc = new PDFDocument();
 
-	const input = fs.createReadStream("combined.pdf");
-	const output = fs.createWriteStream("output.pdf");
-	input.pipe(output);
-
-	pdfDoc.on("pageAdded", () => {
-		pdfDoc.switchToPage(pdfDoc.pageCount - 1);
-		pdfDoc.image("combined.pdf", {
-			fit: [pdfDoc.page.width, pdfDoc.page.height],
-			align: "center",
-			valign: "center",
-            
-		});
-	});
-
-	pdfDoc.on("end", () => {
-		output.end();
-	});
-
-	pdfDoc.end();
-
-	console.log("DONE");
-}
-
-async function usingPDFActions() {
-	var img = fs.readFileSync('combined.pdf');
-
-	// const input = fs.createReadStream("combined.pdf");
-	const pdfDocument = await createPDF.PDFDocumentFromFile(img);
-	// let doc = await addMarginPDF(pdfDocument, [10, 10, 10, 10]);
-	// doc.pipe(fs.createWriteStream("output.pdf"));
+async function usingPDFActions(inputFilePath, outputFilePath) {
+	//load file
+	const img = fs.readFileSync(inputFilePath);
 	
-	console.log("DONE");
+	//convert buffer to blob
+	const blobFile = new Blob([img]);
+	
+	//load bloc on package
+	const pdfDocument = await createPDF.PDFDocumentFromFile(blobFile);
+	//add margins
+	let addedMarginFile = await addMarginPDF(pdfDocument, [10, 10, 10, 10]);
+	const pdfFile = await addedMarginFile.save();
+
+	//write this to file
+	fs.writeFileSync(outputFilePath, pdfFile);
+	
+	console.log("MARGINING DONE");
 }
 
-usingPDFActions();
+// usingPDFActions();
 
 
-// usingPDFKIT()
-module.exports = { usingPDFKIT };
+module.exports = { usingPDFActions };
--- index.js
+++ index.js
@@ -1,42 +1,78 @@
 const {imageCutter} = require("./SubImages");
 const {combineImagesToPDF} = require("./PDF");
 const { generateSequence, getArrayOfSubImagesName } = require("./Numbering");
-
+const { usingPDFActions } = require("./Combine");
+const fs  = require("fs");
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+
+///@NOTICE 
+// scrollshot # 351 and 352 remains in input folder
 
 (async () => {
 	try {
-        const gap = 14;
+        
+        let gap = 14;
         let start = 1;
-        let {end} = generateSequence(gap, start);
+        let end;
+        function increaseStartEnd() {
+            start += gap
+            end += gap
+        }
+        // let {end} = generateSequence(gap, start);
         
         //generate names
-        let SubImagesName = getArrayOfSubImagesName(start, end)
+        // let SubImagesName = getArrayOfSubImagesName(start, end)
 
-        // for(let i = 1; i <= 35; i++){
-        //     let {end} = generateSequence(gap, start);
-        //     console.log(start, end);
-        //     //cut images
-        //     await imageCutter(gap,  start, end, `input/check${i}.jpg`)
-        //     start = end + 1;
-        //     console.log(i);
-        // }
-        // start = 1
+        ///NOTICE: THIS IS THE ONLY LINE THAT NEEDS TO BE CHANGED
+        //we had 352 images, so 14 sub-images of 1 image.
+        //352 * 14 = 4928
+        //we want to generate 10 PDFs, so 4928 / 10 = 492.8 images per PDF
+        
+        //35 scroll shots divided into 14 sub-images of each then all those sub-images 
+        //combined into a single PDFvwhich has 4 images on each page
+        //this loop will run the total number of scrollshots i.e. 352
+        for(let i = 1; i <= 350; i++){
+            //returns start, end and gap
+            let {end} = generateSequence(gap, start);
+            console.log('start',start,' | end',end);
+            //cut images
 
+            //pick image one by one and cut into defined parts
+            await imageCutter(gap,  start, end, `input/image${i}.jpg`)
+            start = end + 1;
+            // console.log(i);
+        }
+        //resetting start and gap for combination 
+        start = 1
+        gap = 490
+        end = gap
+
+        //delaying to avoide any inconsistency while picking sub-images for PDF
         await delay(500)
+        let tempArr = [1,2,3,4,5,6,7,8,9,10]
+        //================================================================================================
+        if (!fs.existsSync('./PDF')){
+            fs.mkdirSync('./PDF');
+        }
+        for(let i = 1; i <= tempArr.length; i++) {
+            //give names to images in array
+            let SubImagesName = getArrayOfSubImagesName(start, end)
+            console.log('start',start,' | end', end)
+            increaseStartEnd()
+            await combineImagesToPDF(SubImagesName, `PDF/${i}.pdf`)            
+        }
+        console.log("PDF MERGE DONE")
+        console.log('STARTING MARGIN');
         
-        //give names to images in array
-        SubImagesName = getArrayOfSubImagesName(start, 480)
-        console.log(SubImagesName);
-		await combineImagesToPDF(SubImagesName, "combined1.pdf")
-        
-        // SubImagesName = getArrayOfSubImagesName(57, 112)
-        // console.log(SubImagesName);
-		// await combineImagesToPDF(SubImagesName, "combined2.pdf")
-        // SubImagesName = getArrayOfSubImagesName(113, 168)
-        // SubImagesName = getArrayOfSubImagesName(start, end)
-
-        //convert images to combined PDF
+        //================================================================================================
+        if (!fs.existsSync('./FINAL')){
+            fs.mkdirSync('./FINAL');
+        }
+        // add margins to generated PDFs
+        for(let i = 9; i <= tempArr.length; i++) {
+            await usingPDFActions(`PDF/${i}.pdf`, `FINAL/${i}.pdf`);
+        }
 
 		console.log("ALL DONE");
 	} catch (error) {
